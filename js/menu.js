255d7865fde10208099843463bbcc3d81560ac21
unrealisher <unrealmpa3@mail.ru>
2018-09-10 14:28:21 +0300

    Сделал проект(нужны изменения)

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,19 @@
 var open = document.querySelector(".page-header__button");
-var menu = document.querySelector(".page-header__main-nav");
+var menu = document.querySelector(".main-nav");
 var close = document.querySelector(".main-nav__close");
+
+menu.classList.remove("main-nav--opened");
 
 open.addEventListener("click", function (evt){
   evt.preventDefault();
-  menu.classList.add("menu-show");
+  if (menu.classList.contains("main-nav--opened")) {
+    menu.classList.remove("main-nav--opened");
+  } else {
+    menu.classList.add("main-nav--opened");
+  }
 });
 
 close.addEventListener("click", function(evt){
   evt.preventDefault();
-  menu.classList.remove("menu-show");
+  menu.classList.remove("main-nav--opened");
 });
